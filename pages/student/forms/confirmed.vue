a65d1fe2fd73892bb27b412d70113c9c4e35b28c
Jirasin46033 <jirasin.46033@mail.kmutt.ac.th>
2022-05-21 16:11:48 +0700

    add validation form

--- pages/student/forms/confirmed.vue
+++ pages/student/forms/confirmed.vue
@@ -7,107 +7,147 @@
       </div>
 
       <div class="mt-10 relative">
-        <div class="bg-gray-200 h-[630px] w-full rounded-lg">
+        <div class="bg-gray-200 h-auto w-full rounded-lg p-5">
           <!-- Row1 -->
-          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2">
-            <div class="w-[700px]">
+          <div class="flex mx-3 justify-between pt-5 pb-2 space-x-2">
+            <div class="w-1/2">
               <label for="" class="block mb-2 text-sm font-bold text-gray-700"
                 >Firstname</label
               >
               <input
+                v-model="confirmedForm.firstname"
                 type="text"
                 placeholder="Firstname"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateFirstname" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
-            <div class="w-[700px]">
+            <div class="w-1/2">
               <label for="" class="block mb-2 text-sm font-bold text-gray-700"
                 >Lastname</label
               >
               <input
+                v-model="confirmedForm.lastname"
                 type="text"
                 placeholder="Lastname"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateLastname" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
           </div>
 
           <!-- Row2 -->
-          <div class="pt-5 pl-5 pr-5 pb-2">
-            <div class="w-[1425px] mx-3">
+          <div class="flex pt-5 pb-2">
+            <div class="w-full mx-3">
               <label for="" class="block mb-2 text-sm font-bold text-gray-700"
                 >Email</label
               >
               <input
+                v-model="confirmedForm.email"
                 type="text"
                 placeholder="Email"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateEmail" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
           </div>
 
           <!-- Row3 -->
-          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2">
-            <div class="w-[700px]">
+          <div class="flex mx-3 justify-between pt-5 pb-2 space-x-2">
+            <div class="w-1/2">
               <label for="" class="block mb-2 text-sm font-bold text-gray-700"
                 >Student ID</label
               >
               <input
+                v-model="confirmedForm.studentid"
                 type="text"
                 placeholder="6xxxxxxxxxx"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateStudentID" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
-            <div class="w-[700px]">
+            <div class="w-1/2">
               <label for="" class="block mb-2 text-sm font-bold text-gray-700"
                 >Telephone</label
               >
               <input
+                v-model="confirmedForm.telephone"
                 type="text"
                 placeholder="0999999999"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateTelephone" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
           </div>
 
           <!-- Row4 -->
-          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2">
-            <div class="w-[700px]">
+          <div class="flex mx-3 justify-between pt-5 pb-2 space-x-2">
+            <div class="w-1/2">
               <label for="" class="block mb-2 text-sm font-bold text-gray-700"
                 >Company name</label
               >
               <input
+                v-model="confirmedForm.companyname"
                 type="text"
                 placeholder="Company name"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateCompanyName" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
-            <div class="w-[700px]">
+            <div class="w-1/2">
               <label for="" class="block mb-2 text-sm font-bold text-gray-700"
                 >Position name</label
               >
               <input
+                v-model="confirmedForm.position"
                 type="text"
                 placeholder="Position name"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validatePosition" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
           </div>
 
           <!-- Row5 -->
-          <div class="pt-5 pl-5 pr-5 pb-2">
-            <div class="w-[1425px] mx-3">
+          <div class="flex pt-5 pb-2">
+            <div class="w-full mx-3">
               <label for="" class="block mb-2 text-sm font-bold text-gray-700"
                 >Duration of training</label
               >
               <input
+                v-model="confirmedForm.duration"
                 type="text"
                 placeholder="2, 4, 6 month"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateDuration" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
           </div>
-          <button class="btn btn-info absolute bottom-0 right-8 text-white w-36 mb-5 hover:outline hover:outline-offset-2 hover:outline-black">Send</button>
+          <div class="flex justify-end mr-5 mt-8">
+            <button
+              @click="checkForm"
+              class="btn btn-info bottom-0 right-8 text-white w-36 mb-5 hover:outline hover:outline-offset-2 hover:outline-black"
+            >
+              Send
+            </button>
+          </div>
+          <!-- <button @click="checkForm" class="btn btn-info absolute bottom-0 right-8 text-white w-36 mb-5 hover:outline hover:outline-offset-2 hover:outline-black" >Send</button> -->
         </div>
       </div>
     </div>
@@ -116,7 +156,50 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      confirmedForm: {
+        firstname: '',
+        lastname: '',
+        email: '',
+        studentid: '',
+        telephone: '',
+        companyname: '',
+        position: '',
+        duration: '',
+      },
+      validateFirstname: true,
+      validateLastname: true,
+      validateEmail: true,
+      validateStudentID: true,
+      validateTelephone: true,
+      validateCompanyName: true,
+      validatePosition: true,
+      validateDuration: true,
+    }
+  },
+
+  methods: {
+    checkForm() {
+      this.validateFirstname =
+        this.confirmedForm.firstname.length === 0 ? false : true
+      this.validateLastname =
+        this.confirmedForm.lastname.length === 0 ? false : true
+      this.validateEmail = this.confirmedForm.email.length === 0 ? false : true
+      this.validateStudentID =
+        this.confirmedForm.studentid.length === 0 ? false : true
+      this.validateTelephone =
+        this.confirmedForm.telephone.length === 0 ? false : true
+      this.validateCompanyName =
+        this.confirmedForm.companyname.length === 0 ? false : true
+      this.validatePosition =
+        this.confirmedForm.position.length === 0 ? false : true
+      this.validateDuration =
+        this.confirmedForm.duration.length === 0 ? false : true
+    },
+  },
+}
 </script>
 
 <style></style>
--- pages/student/forms/internship.vue
+++ pages/student/forms/internship.vue
@@ -7,109 +7,176 @@
       </div>
 
       <div class="mt-10 relative">
-        <div class="bg-gray-200 h-[630px] w-full rounded-lg">
+        <div class="bg-gray-200 h-auto w-full rounded-lg">
           <!-- Row1 -->
-          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2">
-            <div class="w-[700px]">
-              <label for="" class="block mb-2 text-sm font-bold text-gray-700">Firstname</label>
+          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2 space-x-2">
+            <div class="w-1/2">
+              <label for="" class="block mb-2 text-sm font-bold text-gray-700"
+                >Firstname</label
+              >
               <input
+                v-model="forms.firstname"
                 type="text"
                 placeholder="Firstname"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateFirstname" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
-            <div class="w-[700px]">
-              <label for="" class="block mb-2 text-sm font-bold text-gray-700">Lastname</label>
+            <div class="w-1/2">
+              <label for="" class="block mb-2 text-sm font-bold text-gray-700"
+                >Lastname</label
+              >
               <input
+                v-model="forms.lastname"
                 type="text"
                 placeholder="Lastname"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateLastname" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
           </div>
 
           <!-- Row2 -->
-          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2">
-            <div class="w-[700px]">
-              <label for="" class="block mb-2 text-sm font-bold text-gray-700">Email</label>
+          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2 space-x-2">
+            <div class="w-1/2">
+              <label for="" class="block mb-2 text-sm font-bold text-gray-700"
+                >Email</label
+              >
               <input
+                v-model="forms.email"
                 type="text"
                 placeholder="Email"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateEmail" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
-            <div class="w-[700px]">
-              <label for="" class="block mb-2 text-sm font-bold text-gray-700">Telephone</label>
+            <div class="w-1/2">
+              <label for="" class="block mb-2 text-sm font-bold text-gray-700"
+                >Telephone</label
+              >
               <input
+                v-model="forms.telephone"
                 type="text"
                 placeholder="0999999999"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateTelephone" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
           </div>
 
           <!-- Row3 -->
-          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2">
-            <div class="w-[700px]">
-              <label for="" class="block mb-2 text-sm font-bold text-gray-700">Year</label>
+          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2 space-x-2">
+            <div class="w-1/2">
+              <label for="" class="block mb-2 text-sm font-bold text-gray-700"
+                >Year</label
+              >
               <input
+                v-model="forms.year"
                 type="text"
                 placeholder="1, 2, 3, 4"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateYear" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
-            <div class="w-[700px]">
-              <label for="" class="block mb-2 text-sm font-bold text-gray-700">GPAX</label>
+            <div class="w-1/2">
+              <label for="" class="block mb-2 text-sm font-bold text-gray-700"
+                >GPAX</label
+              >
               <input
+                v-model="forms.gpax"
                 type="text"
                 placeholder="x.xx"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateGpax" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
           </div>
 
           <!-- Row4 -->
-          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2">
-            <div class="w-[700px]">
-              <label for="" class="block mb-2 text-sm font-bold text-gray-700">Company name</label>
+          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2 space-x-2">
+            <div class="w-1/2">
+              <label for="" class="block mb-2 text-sm font-bold text-gray-700"
+                >Company name</label
+              >
               <input
+                v-model="forms.companyname"
                 type="text"
                 placeholder="Company name"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateCompanyName" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
-            <div class="w-[700px]">
-              <label for="" class="block mb-2 text-sm font-bold text-gray-700">Position name</label>
+            <div class="w-1/2">
+              <label for="" class="block mb-2 text-sm font-bold text-gray-700"
+                >Position name</label
+              >
               <input
+                v-model="forms.position"
                 type="text"
                 placeholder="Software Developer, UX/UI Designer, BA, etc."
                 class="input input-bordered w-full"
               />
+              <p v-if="!validatePosition" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
           </div>
 
           <!-- Row5 -->
-          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2">
-            <div class="w-[700px]">
-              <label for="" class="block mb-2 text-sm font-bold text-gray-700">My address</label>
+          <div class="flex mx-3 justify-between pt-5 pl-5 pr-5 pb-2 space-x-2">
+            <div class="w-1/2">
+              <label for="" class="block mb-2 text-sm font-bold text-gray-700"
+                >My address</label
+              >
               <input
+                v-model="forms.myaddress"
                 type="text"
                 placeholder="My address"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateMyAddress" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
-            <div class="w-[700px]">
-              <label for="" class="block mb-2 text-sm font-bold text-gray-700">My resume (Link)</label>
+            <div class="w-1/2">
+              <label for="" class="block mb-2 text-sm font-bold text-gray-700"
+                >My resume (Link)</label
+              >
               <input
+                v-model="forms.myresume"
                 type="text"
                 placeholder="Link Google Drive"
                 class="input input-bordered w-full"
               />
+              <p v-if="!validateMyResume" class="text-red-700 mt-2">
+                This field need requied
+              </p>
             </div>
           </div>
-        </div>
 
-        <button class="btn btn-info absolute bottom-0 right-8 text-white w-36 mb-5 hover:outline hover:outline-offset-2 hover:outline-black">Send</button>
+          <div class="flex justify-end mr-8 mt-8">
+            <button
+              @click="checkForm"
+              class="btn btn-info bottom-0 right-8 text-white w-36 mb-5 hover:outline hover:outline-offset-2 hover:outline-black"
+            >
+              Send
+            </button>
+          </div>
+        </div>
       </div>
     </div>
     <Footer />
@@ -117,7 +184,50 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      forms: {
+        firstname: '',
+        lastname: '',
+        email: '',
+        telephone: '',
+        year: '',
+        gpax: '',
+        companyname: '',
+        position: '',
+        myaddress: '',
+        myresume: '',
+      },
+      validateFirstname: true,
+      validateLastname: true,
+      validateEmail: true,
+      validateTelephone: true,
+      validateYear: true,
+      validateGpax: true,
+      validateCompanyName: true,
+      validatePosition: true,
+      validateMyAddress: true,
+      validateMyResume: true,
+    }
+  },
+
+  methods: {
+    checkForm() {
+      this.validateFirstname = this.forms.firstname.length === 0 ? false : true
+      this.validateLastname = this.forms.lastname.length === 0 ? false : true
+      this.validateEmail = this.forms.email.length === 0 ? false : true
+      this.validateTelephone = this.forms.telephone.length === 0 ? false : true
+      this.validateYear = this.forms.year.length === 0 ? false : true
+      this.validateGpax = this.forms.gpax.length === 0 ? false : true
+      this.validateCompanyName =
+        this.forms.companyname.length === 0 ? false : true
+      this.validatePosition = this.forms.position.length === 0 ? false : true
+      this.validateMyAddress = this.forms.myaddress.length === 0 ? false : true
+      this.validateMyResume = this.forms.myresume.length === 0 ? false : true
+    },
+  },
+}
 </script>
 
 <style></style>
