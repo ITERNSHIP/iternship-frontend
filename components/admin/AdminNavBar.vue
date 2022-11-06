ea00dc7ae9390519dfe1407b6c1c2a0c22b35191
Jirasin46033 <jirasin.46033@mail.kmutt.ac.th>
2022-11-07 00:30:50 +0700

    [update] create staff account UI component and AdminNavBar component

---
+++ components/admin/AdminNavBar.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="navbar bg-black-gray rounded-none justify-between">
+    <div class="px-4 w-full">
+      <div class="flex cursor-pointer ">
+        <!-- <a class="btn btn-ghost normal-case text-xl text-cheese">ITERNSHIP</a> -->
+        <img src="@/static/logo.png" alt="##" class="w-16">
+      </div>
+
+      <div class="lg:flex ml-auto space-x-6 hidden pr-2">
+        
+        <button v-if="`${this.$cookiz.get('jwt')}`" class="btn text-white w-32" @click="logout">ออกจากระบบ</button>
+      </div>
+
+      <!-- Mobile Hambeger -->
+      <!-- <div class="dropdown dropdown-left flex flex-none md:hidden">
+        <button tabindex="0" class="btn-square btn-info">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block w-6 h-6 stroke-current"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+        <ul
+          tabindex="0"
+          class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <nuxt-link to="/student" class="text-black  font-bold">Home</nuxt-link>
+          <li class="text-black font-bold">News</li>
+          <nuxt-link to="/student/forms" class="text-black  font-bold"
+            >Forms</nuxt-link
+          >
+          <li class="text-black  font-bold">About</li>
+        </ul>
+      </div> -->
+    </div>
+
+    <div class="dropdown dropdown-end cursor-pointer lg:hidden pr-6">
+        <button tabindex="0">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/7711/7711100.png"
+            class="h-5 w-5"
+            alt="hambuger"
+          />
+        </button>
+        <ul
+          tabindex="0"
+          class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <!-- <li><a>เข้าสู่ระบบ</a></li> -->
+          <li><a @click="logout()">ออกจากระบบ</a></li>
+        </ul>
+      </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+    logout() {
+      this.$cookiz.remove('jwt')
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('role')
+      this.$router.push('/')
+      alert('ออกจากระบบสำเร็จ')
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
--- pages/admin/management.vue
+++ pages/admin/management.vue
@@ -1,15 +1,292 @@
 <template>
   <div>
-    test
+    <AdminNavBar />
+    <section class="mt-8">
+      <div class="px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:items-center">
+          <div class="sm:flex-auto">
+            <h1 class="text-2xl font-bold text-gray-900">จัดการบัญชีบริษัท</h1>
+            <!-- <p class="mt-2 text-sm text-gray-700">
+              A list of all the users in your account including their name,
+              title, email and role.
+            </p> -->
+          </div>
+          <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+            <label
+              for="add"
+              class="inline-flex items-center justify-center rounded-md border border-transparent bg-blue-blue px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:w-auto"
+            >
+              สร้างบัญชีเจ้าหน้าที่
+            </label>
+            <input type="checkbox" id="add" class="modal-toggle" />
+            <div class="modal">
+              <div class="modal-box relative w-11/12 max-w-5xl">
+                <label
+                  for="add"
+                  class="btn btn-sm btn-circle absolute right-2 top-2"
+                  >✕</label
+                >
+                <h3 class="text-lg font-bold mb-4">สร้างบัญชีเจ้าหน้าที่</h3>
+
+                <div class="mb-4">
+                  <label
+                    class="text-gray-800 text-sm font-normal leading-tight tracking-normal"
+                    >ชื่อจริง</label
+                  >
+                  <input
+                    v-model.trim.lazy="$v.firstName.$model"
+                    type="text"
+                    class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-blue-blue font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
+                    placeholder="ชื่อจริง"
+                  />
+                  <p
+                    v-if="!$v.firstName.required && $v.firstName.$dirty"
+                    class="text-error text-sm mt-2"
+                  >
+                    กรุณาระบุข้อมูล
+                  </p>
+                </div>
+
+                <div class="mb-4">
+                  <label
+                    class="text-gray-800 text-sm font-normal leading-tight tracking-normal"
+                    >นามสกุล</label
+                  >
+                  <input
+                    v-model.trim.lazy="$v.lastName.$model"
+                    type="text"
+                    class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-blue-blue font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
+                    placeholder="นามสกุล"
+                  />
+                  <p
+                    v-if="!$v.lastName.required && $v.lastName.$dirty"
+                    class="text-error text-sm mt-2"
+                  >
+                    กรุณาระบุข้อมูล
+                  </p>
+                </div>
+
+                <div class="mb-4">
+                  <label
+                    class="text-gray-800 text-sm font-normal leading-tight tracking-normal"
+                    >อีเมล</label
+                  >
+                  <input
+                    v-model.trim.lazy="$v.staffEmail.$model"
+                    type="email"
+                    class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-blue-blue font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
+                    placeholder="อีเมล"
+                  />
+                  <p
+                    v-if="!$v.staffEmail.required && $v.staffEmail.$dirty"
+                    class="text-error text-sm mt-2"
+                  >
+                    กรุณาระบุข้อมูล
+                  </p>
+                  <p
+                    v-if="!$v.staffEmail.email && $v.staffEmail.$dirty"
+                    class="text-error text-sm mt-2"
+                  >
+                    กรุณาระบุข้อมูลเป็นอีเมล
+                  </p>
+                </div>
+
+                <div class="mb-4">
+                  <label
+                    class="text-gray-800 text-sm font-normal leading-tight tracking-normal"
+                    >รหัสผ่าน</label
+                  >
+                  <input
+                    v-model.trim.lazy="$v.password.$model"
+                    type="password"
+                    class="mt-2 text-gray-600 focus:outline-none focus:border focus:border-blue-blue font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
+                    placeholder="รหัสผ่าน"
+                  />
+                  <p
+                    v-if="!$v.password.required && $v.password.$dirty"
+                    class="text-error text-sm mt-2"
+                  >
+                    กรุณาระบุข้อมูล
+                  </p>
+                  <p
+                    v-if="!$v.password.minLength && $v.password.$dirty"
+                    class="text-error text-sm mt-2"
+                  >
+                    กรุณาระบุรหัสผ่านอย่างน้อย 8 ตัวอักษร
+                  </p>
+                </div>
+
+                <div class="modal-action flex items-center justify-end w-full">
+                  <label
+                    class="btn btn-info text-white"
+                    @click="createStaffAccount"
+                    >สร้างบัญชี</label
+                  >
+                  <label for="add" class="btn">ยกเลิก</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="-mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg"
+        >
+          <table class="min-w-full divide-y divide-gray-300">
+            <thead class="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                >
+                  ชื่อผู้ติดต่อ
+                </th>
+                <th
+                  scope="col"
+                  class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                >
+                  เบอร์โทรศัพท์
+                </th>
+                <th
+                  scope="col"
+                  class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                >
+                  ชื่อบริษัท
+                </th>
+                <th
+                  scope="col"
+                  class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  สถานะ
+                </th>
+                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                  <span class="sr-only">เปลี่ยนสถานะ</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200 bg-white">
+              <tr v-for="person in people" :key="person.email">
+                <td
+                  class="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6"
+                >
+                  {{ person.name }}
+                  <dl class="font-normal lg:hidden">
+                    <dt class="sr-only">Title</dt>
+                    <dd class="mt-1 truncate text-gray-700">
+                      {{ person.title }}
+                    </dd>
+                    <dt class="sr-only sm:hidden">Email</dt>
+                    <dd class="mt-1 truncate text-gray-500 sm:hidden">
+                      {{ person.email }}
+                    </dd>
+                  </dl>
+                </td>
+                <td
+                  class="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell"
+                >
+                  {{ person.title }}
+                </td>
+                <td
+                  class="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell"
+                >
+                  {{ person.email }}
+                </td>
+                <td class="px-3 py-4 text-sm text-gray-500"
+                :class="[ person.status == 'ปกติ' ? 'text-info' : 'text-red-500' ] ">
+                  {{ person.status }}
+                </td>
+                <td
+                  class="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6"
+                >
+                  <p
+                    @click="changeCompanyStatus()"
+                    class="text-blue-600 hover:text-blue-900"
+                  >
+                    เปลี่ยนสถานะ
+                  </p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import AdminNavBar from '~/components/admin/AdminNavBar.vue'
+import { required, email, minLength } from 'vuelidate/lib/validators'
 export default {
-
+  components: { AdminNavBar },
+  data() {
+    return {
+      people: [
+        {
+          name: 'เจเจ เจเจ',
+          title: 'Co-Founder / CEO',
+          email: 'test@tet.com',
+          status: 'ปกติ',
+        },
+        {
+          name: 'เจเจ เจเจ',
+          title: 'Co-Founder / CEO',
+          email: 'test@tet.com',
+          status: 'ระงับการใช้งาน',
+        },
+      ],
+      firstName: '',
+      lastName: '',
+      staffEmail: '',
+      password: '',
+    }
+  },
+  methods: {
+    createStaffAccount() {
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        return
+      } else {
+        const data = {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          staffEmail: this.staffEmail,
+          password: this.password,
+        }
+        console.log(
+          '🚀 ~ file: management.vue ~ line 264 ~ createStaffAccount ~ data',
+          data
+        )
+        // this.$axios
+        //   .$post('/api/staff/create', data)
+        //   .then((res) => {
+        //     console.log(res)
+        //   })
+        //   .catch((err) => {
+        //     console.log(err)
+        //   })
+      }
+    },
+    changeCompanyStatus() {
+      console.log('change status')
+    },
+  },
+  validations: {
+    firstName: {
+      required,
+    },
+    lastName: {
+      required,
+    },
+    staffEmail: {
+      required,
+      email,
+    },
+    password: {
+      required,
+      minLength: minLength(8),
+    },
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
